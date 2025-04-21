34e94b5feacb474992e886fc1dda933a7f1c1130
mino <k3000js@naver.com>
2025-04-21 16:44:31 +0900

    [Feature] - settings 수정

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,6 +20,13 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Assignment"
+rootProject.name = "assignment"
 include(":app")
- 
+include(":feature:commerce")
+include(":feature:main")
+include(":core:designsystem")
+include(":core:model")
+include(":server")
+include(":data")
+include(":domain")
+include(":datastore")
